0f1c88c3d263a4e42b6509d980167103e90d5bb4
ram1k <zo0zo0@mail.ru>
2023-06-11 13:26:31 +0300

    1st lesson - all 4 tasks

---
+++ Task_1.2.py
@@ -0,0 +1,19 @@
+"""Задача 2: Найдите сумму цифр трехзначного числа.
+
+*Пример:*
+
+123 -> 6 (1 + 2 + 3)
+100 -> 1 (1 + 0 + 0) 
+"""
+
+
+def amount():
+    user_imp = input('Введите 3 любых цифры: ')
+    sum = 0
+    for el in user_imp:
+        sum = sum + int(el)
+    return sum
+
+
+if __name__ == "__main__":
+    print(f'Результат:{amount()}')
